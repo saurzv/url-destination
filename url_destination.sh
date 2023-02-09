fdbd029a1a83a37069af1b118ba894eae9a14879
Saurav Raj <saurav.raj5592+git@gmail.com>
2023-02-09 15:50:15 +0530

    Add method to check for broken links

--- url_destination.sh
+++ url_destination.sh
@@ -4,13 +4,13 @@ checkForCurl(){
     if command -v curl >> /dev/null; then
         return 0
     else
-        echo "ERROR: curl is required!"
+        echo "\n\tERROR: curl is required!\n"
         return 1
     fi
 }
 
 isActiveInternet(){
-    curl -ILso /dev/null github.com  2>&1 || { echo -e "\n\tERROR: Active internet connection needed!\n" >&2; return 1; }
+    curl -ILso /dev/null github.com  2>&1 || { echo "\n\tERROR: Active internet connection needed!\n >&2"; return 1; }
 }
 
 isValidURL(){
@@ -18,13 +18,21 @@ isValidURL(){
     if [[ "$@" =~ $regex ]]; then
         return 0
     else
-        echo "URL is not valid!"
+        echo -e "\n\tURL is not valid!\n"
         return 1
     fi
 }
 
 getDestination(){
-    curl -ILso /dev/null -w "\n\tDestination URL : %{url_effective}\n\n" "$@"
+    curl_rsp="$(curl -ILso /dev/null -w "%{http_code} %{url_effective}" "$@")"
+    rsp_code="$(echo $curl_rsp | awk '{print $1}')"
+    url_dest="$(echo $curl_rsp | awk '{print $2}')"
+
+    if [[ "$rsp_code" == 200 ]]; then
+        echo -e "\n\tURL Destination : $url_dest\n"
+    else
+        echo -e "\n\tERROR: URL might be broken :(\n\tStatus Code: $rsp_code\n"
+    fi
 }
 
 if [[ "$#" == 0 ]]; then
